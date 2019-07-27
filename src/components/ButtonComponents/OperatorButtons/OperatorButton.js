7fa7e54829c988ab5074ef5189e8c5412b85300b
Mada Gower <gmgower@gmail.com>
2019-07-26 21:25:34 -0500

    Add styles to operator buttons.

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -3,7 +3,7 @@ import React from "react";
 const OperatorButton = (props) => {
   console.log({props})
   return (
-    <button>
+    <button class="operator-button">
             {/* Display a button element rendering the data being passed down from the parent container on props */}
       {props.operator.char}
     </button>
