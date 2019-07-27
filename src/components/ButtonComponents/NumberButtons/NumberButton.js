211a4c77a4df82ece766520e87adf041295a5da0
Mada Gower <gmgower@gmail.com>
2019-07-26 22:20:39 -0500

    Add styles to zero button.

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,8 +1,9 @@
 import React from "react";
 
 const NumberButton = (props) => {
+  console.log("NumberButton", props)
   return (
-    <button className="number-button">
+    <button className={`number-button ${props.text == 0 ? 'zero' : ''}`}>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
     {props.text}
     </button>
